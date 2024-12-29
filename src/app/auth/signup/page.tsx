af9fd42b2989d435a92975ed90a046f7700e63d2
neko-no <101441574+neko-no@users.noreply.github.com>
2024-12-29 18:14:53 +0900

    #2 fix: inputのコンポーネントを汎用化

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -6,9 +6,24 @@ const page = () => {
     <div className="mx-auto max-w-sm my-14">
       <h2 className="text-center font-medium text-2xl mb-4">新規登録</h2>
       <form>
-        <InputField />
-        <InputField />
-        <InputField />
+        <InputField
+          label="ユーザー名"
+          type="text"
+          placeholder="ユーザー名"
+          name="username"
+        />
+        <InputField
+          label="メールアドレス"
+          type="email"
+          placeholder="メールアドレス"
+          name="email"
+        />
+        <InputField
+          label="パスワード"
+          type="password"
+          placeholder="パスワード"
+          name="password"
+        />
         <div className="mt-4">
           <button>送信</button>
         </div>
