5b837e7dc57a37463392a83ae2ff7b1ac27e46be
neko-no <101441574+neko-no@users.noreply.github.com>
2024-12-31 17:29:01 +0900

    #3 add: supabaseでの認証ができるように変更

--- src/app/features/auth/hooks/useSignupForm.ts
+++ src/app/features/auth/hooks/useSignupForm.ts
@@ -2,9 +2,12 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { SubmitHandler, useForm } from "react-hook-form"
 import { signupFormSchema } from "../lib/formSchema";
 import { z } from "zod";
+import { supabase } from "../lib/supabaseClient";
+import { useRouter } from "next/navigation";
 
 
 export const useSignupForm = () => {
+    const router = useRouter();
     const form = useForm<z.infer<typeof signupFormSchema>>({
         resolver: zodResolver(signupFormSchema),
         defaultValues: {
@@ -14,9 +17,26 @@ export const useSignupForm = () => {
         }
     });
 
-    const onSubmit: SubmitHandler<z.infer<typeof signupFormSchema>> = (data) => {
-        const {username, email, password} = data;
-        console.log(username, email, password);
+    const onSubmit: SubmitHandler<z.infer<typeof signupFormSchema>> = async (data) => {
+        const {email, password} = data;
+        // signup
+        try {
+            const { error: signUpError } = await supabase.auth.signUp({
+                email,
+                password,
+              })
+
+            if (signUpError) {
+                console.log(signUpError);
+                throw signUpError;
+            }
+
+            router.push("/auth/login");
+        } catch (err) {
+            if(err instanceof Error){
+                console.log(err.message)
+            }
+        }
     }
 
     return { form, onSubmit}
